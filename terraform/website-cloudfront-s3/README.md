3e6b389a590dc15543ffffb709ecd1741d56465c
Rishabh <grishabh1992@gmail.com>
2021-01-04 23:31:39 +0530

    Added script for deploying web

---
+++ terraform/website-cloudfront-s3/README.md
@@ -0,0 +1,4 @@
+# Terraform Script for Deploy Website using Cloudfront
+
+1. Create a public bucket
+2. Create cloudfront distribution and add s3 bucket as origin 
---
+++ terraform/website-cloudfront-s3/main.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+variable "www_domain_name" {
+  default = "www.rishabh-web.io"
+}
+
+// We'll also need the root domain (also known as zone apex or naked domain).
+variable "root_domain_name" {
+  default = "rishabh-web.io"
+}
+
+resource "aws_s3_bucket" "web_storage" {
+  bucket = var.www_domain_name
+  acl    = "public-read"
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AddPerm",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.www_domain_name}/*"]
+    }
+  ]
+}
+POLICY
+
+  website {
+    // Here we tell S3 what to use when a request comes in to the root
+    // ex. https://www.runatlantis.io
+    index_document = "index.html"
+  }
+  tags = {
+    Name = "Web Storage"
+  }
+}
+
+resource "aws_cloudfront_distribution" "www_distribution" {
+  // origin is where CloudFront gets its content from.
+  origin {
+    // We need to set up a "custom" origin because otherwise CloudFront won't
+    // redirect traffic from the root domain to the www domain, that is from
+    // runatlantis.io to www.runatlantis.io.
+    custom_origin_config {
+      // These are all the defaults.
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+
+    // Here we're using our S3 bucket's URL!
+    domain_name = aws_s3_bucket.www.website_endpoint
+    // This can be any name to identify this origin.
+    origin_id = var.www_domain_name
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  // All values are defaults from the AWS console.
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    // This needs to match the `origin_id` above.
+    target_origin_id = var.www_domain_name
+    min_ttl          = 0
+    default_ttl      = 86400
+    max_ttl          = 31536000
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  // Here we're ensuring we can hit this distribution using www.runatlantis.io
+  // rather than the domain name CloudFront gives us.
+  # aliases = [var.www_domain_name]
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  // Here's where our certificate is loaded in!
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
